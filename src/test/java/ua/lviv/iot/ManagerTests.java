55831395c5d6f19d4345f093f5ac297eb2f4d5ef
dariy-vel <dariy.velychko@gmail.com>
2019-02-20 11:19:38 +0200

    Improved tests quality

--- src/test/java/ua/lviv/iot/ManagerTests.java
+++ src/test/java/ua/lviv/iot/ManagerTests.java
@@ -1,5 +1,7 @@
 package ua.lviv.iot;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ua.lviv.iot.managers.GardenToolManager;
 import ua.lviv.iot.managers.GardenToolManagerImpl;
@@ -20,58 +22,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ManagerTests {
-    private GardenTool object1 = new Saw(
-            1,
-            1,
-            "Ukraine",
-            "ZAZ",
-            "metal",
-            1,
-            Purpose.WOOD_N_BRANCHES,
-            1,
-            DriveType.PETROL
-    );
-    private GardenTool object2 = new Shovel(
-            2,
-            2,
-            "Ukraine",
-            "ZAZ",
-            "metal",
-            2,
-            Purpose.GROUND,
-            2
-    );
-    private GardenTool object3 = new Averruncator(
-            3,
-            3,
-            "Ukraine",
-            "ZAZ",
-            "metal",
-            3,
-            Purpose.WOOD_N_BRANCHES,
-            Hand.BOTH,
-            3
-    );
-    private GardenTool object4 = new Axe(
-            4,
-            4,
-            "Ukraine",
-            "ZAZ",
-            "metal",
-            4,
-            Purpose.GROUND,
-            4
-    );
+    private GardenTool object1;
+    private GardenTool object2;
+    private GardenTool object3;
+    private GardenTool object4;
     private List<GardenTool> input;
     private GardenToolManager manager;
 
-    {
+    @BeforeEach
+    void setup() {
+        this.object1 = new Saw(
+                1,
+                1,
+                "Ukraine",
+                "ZAZ",
+                "metal",
+                1,
+                Purpose.WOOD_N_BRANCHES,
+                1,
+                DriveType.PETROL
+        );
+        this.object2 = new Shovel(
+                2,
+                2,
+                "Ukraine",
+                "ZAZ",
+                "metal",
+                2,
+                Purpose.GROUND,
+                2
+        );
+        this.object3 = new Averruncator(
+                3,
+                3,
+                "Ukraine",
+                "ZAZ",
+                "metal",
+                3,
+                Purpose.WOOD_N_BRANCHES,
+                Hand.BOTH,
+                3
+        );
+        this.object4 = new Axe(
+                4,
+                4,
+                "Ukraine",
+                "ZAZ",
+                "metal",
+                4,
+                Purpose.GROUND,
+                4
+        );
+
         input = new ArrayList<>();
         input.add(object3);
         input.add(object2);
         input.add(object1);
         input.add(object4);
         manager = new GardenToolManagerImpl(input);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        input.clear();
+
     }
 
     @Test
